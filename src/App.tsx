d735f5ddf9b72c95f4a0aac1595b19a8c1624755
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-15 21:54:24 +0300

    feat: basic project template 08-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import {Lesson8} from './Tuesday/Todolist8/Lesson8';
 
-import {Lesson7} from './Tuesday/Todolist7/Lesson7';
-import {Lesson7Reducers} from './Tuesday/Todolist7Reducers/Lesson7Reducers';
-import {Lesson6} from './Tuesday/Todolist10/Lesson6';
 
 function App() {
 
     return (
         <div>
-            <Lesson6/>
+            <Lesson8/>
         </div>
     );
 }
