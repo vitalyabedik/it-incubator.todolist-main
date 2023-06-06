ff72620d231317288f8ec38f2075ebf14b725403
Vitali Bedik <vitalya.bedik@gmail.com>
2023-06-06 23:11:37 +0300

    feat: basic project template 11-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import {Provider} from 'react-redux';
 
 import './App.css';
 
-import {Lesson10} from './Wednesday/Todolist10/Lesson10';
-import {Lesson10WithReducers} from './Wednesday/Todolist10/Lesson10WithReducers';
-import {Lesson10WithRedux} from './Wednesday/Todolist10/Lesson10WithRedux';
 import {store} from './Wednesday/Todolist10/state/store';
+import {Lesson11} from './Wednesday/Todolist11/Lesson11';
 
 function App() {
 
     return (
         <div>
             <Provider store={store}>
-                <Lesson10WithRedux/>
+                <Lesson11/>
             </Provider>
         </div>
     );
